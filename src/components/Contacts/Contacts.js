8281bfffcb007c6090ddbf460156e6c481feba63
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-11-15 01:41:55 +0200

    old version fix

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,30 +1,18 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import s from './Contacts.module.css';
 import ContactsItem from '../ContactsItem/ContactsItem';
-import Loader from '../Loader/Loader';
-import { getLoading, getVisibleContacts } from '../../redux/selectors';
-import * as operations from '../../redux/operations';
+import { getVisibleContacts } from '../../redux/selectors';
 
 export default function Contacts() {
   const contacts = useSelector(getVisibleContacts);
-  const isLoading = useSelector(getLoading);
-  const dispatch = useDispatch();
-  useEffect(() => dispatch(operations.fetchContacts()), [dispatch]);
 
   return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <ul className={s.contacts__list}>
-          {contacts.map(({ id, name, number }) => (
-            <li key={id} className={s.contacts__item}>
-              <ContactsItem id={id} name={name} number={number} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
+    <ul className={s.contacts__list}>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id} className={s.contacts__item}>
+          <ContactsItem id={id} name={name} number={number} />
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,10 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getEntities = state => state.contacts.entities;
 export const getFilter = state => state.contacts.filter;
 export const getLoading = state => state.contacts.isLoading;
-export const getVisibleContacts = state => {
-  const items = getEntities(state);
-  const filter = getFilter(state);
-  return items.filter(item => item.name.toLowerCase().includes(filter));
-};
+export const getVisibleContacts = createSelector(
+  [getEntities, getFilter],
+  (items, filter) =>
+    items.filter(item => item.name.toLowerCase().includes(filter)),
+);
